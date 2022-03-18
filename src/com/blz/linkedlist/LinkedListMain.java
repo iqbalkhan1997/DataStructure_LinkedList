68ffeeab4cb374a94148714007e8dc31153a5ce3
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-18 10:59:10 +0530

    Added:simple linked list functionality

--- src/com/blz/linkedlist/LinkedListMain.java
+++ src/com/blz/linkedlist/LinkedListMain.java
@@ -1,8 +1,13 @@
 package com.blz.linkedlist;
-
+import java.util.LinkedList;
 public class LinkedListMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to data structure's linkedList program");
+        LinkedList <Integer> list = new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);
     }
 }
