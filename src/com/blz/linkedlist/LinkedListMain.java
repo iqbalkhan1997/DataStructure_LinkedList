22de68392f7d3bdd8ba21afc8907faee6ad46e51
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-18 10:54:17 +0530

    Added welcome message

---
+++ src/com/blz/linkedlist/LinkedListMain.java
@@ -0,0 +1,8 @@
+package com.blz.linkedlist;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to data structure's linkedList program");
+    }
+}
